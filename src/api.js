ec1fa72893d065dbd87f60218684ab9d370b7622
Amit Kumar <103894389+Amit-Rohila33@users.noreply.github.com>
2022-12-07 16:12:34 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "406754da5emsh6575f54b6329935p1e6a7bjsnf9266b2c01d2",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "ce089539362f84405dfc2d62a9153868"
+
+
